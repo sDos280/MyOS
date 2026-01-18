86db7ee203d21fcf542ac250ec6a0ffc15056cbf
dor <dor.shapira@outlook.com>
2026-01-18 17:52:42 +0200

    remove scheduler start thread

--- src/multitasking/process.c
+++ src/multitasking/process.c
@@ -44,7 +44,7 @@ process_t * process_create(void (*entry)(void), size_t stack_size) {
     memset(process, 0, sizeof(process_t));
 
     process->pid = next_pid++;
-    process->status = PROCESS_RUNNING;
+    process->status = PROCESS_NEW;
     process->esp = (uint32_t *)((uint32_t)kalloc(stack_size) + (stack_size - 1));
     /* Since we don't don't *call* entry we just use *ret* when entry is in the stack top, 
        the *ret* in the entry function would pop sheudler_thread_exit and redirect code to there */
