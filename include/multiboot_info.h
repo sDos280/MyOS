54632da0fbc49aed7eea5ee04ebe67cef56f5eae
dor <dor.shapira@outlook.com>
2026-05-10 18:52:06 +0300

    marge multiboot info and test to one file

--- include/multiboot_info.h
+++ include/multiboot_info.h
@@ -1,10 +1,10 @@
-#ifndef MULTIBOOT_HELPER_H
-#define MULTIBOOT_HELPER_H
+#ifndef MULTIBOOT_INFO_H
+#define MULTIBOOT_INFO_H
 
 #include "multiboot.h"
 #include "types.h"
 
-void multiboot_print_info(uint32_t magic, const multiboot_info_t *mbi);
-void multiboot_helper_invalidate_unavailable_memory(multiboot_info_t *mbi);  // invalidate (by setting as used in pmm) unavailable memory 
+void multiboot_info_print(uint32_t magic, const multiboot_info_t *mbi);
+void multiboot_info_invalidate_unavailable_memory(multiboot_info_t *mbi);  // invalidate (by setting as used in pmm) unavailable memory 
 
-#endif // MULTIBOOT_HELPER_H
+#endif // MULTIBOOT_INFO_H
